57978638a499072403e7006561e2efae10875331
Lorenzobattistela <lorenzobattistela@gmail.com>
2025-04-02 19:42:38 -0300

    brute force w loops

--- backpack.py
+++ backpack.py
@@ -42,8 +42,6 @@ def backpackPD(n, c, itens):
                 maxTab[i][j] = maxTab[i-1][j]
     return maxTab[n][c]
 
-
-# brute force - naive
 def backpack_brute_force(num, cap, itens):
     if num == 0 or cap == 0:
         return 0
@@ -54,6 +52,21 @@ def backpack_brute_force(num, cap, itens):
     else:
         return max(itens[num-1][1] + backpack_brute_force(num-1, cap-itens[num-1][0], itens),
                    backpack_brute_force(num-1, cap, itens))
+
+
+def backpack_brute_force_loops(num, cap, itens):
+    max_value = 0
+    for i in range(1<<num):
+        value = 0
+        weight = 0
+        for j in range(num):
+            if i & (1<<j):
+                value += itens[j][1]
+                weight += itens[j][0]
+        if weight <= cap and value > max_value:
+            max_value = value
+    return max_value
+
 
 def backpack_divide_conquer(num, cap, itens):
     if num == 0 or cap == 0:
@@ -72,12 +85,18 @@ def main():
     pesos   = [23, 31, 29, 44, 53, 38, 63, 85, 89, 82]
     valores = [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
     items = list(zip(pesos, valores))
-    print(items)
-    a = backpackPD(len(items), 165, items)
-    b = backpack_brute_force(len(items), 165, items)
-    c = backpack_divide_conquer(len(items), 165, items)
-    print(a)
-    print(b)
-    print(c)
+    #  print(items)
+    #  a = backpackPD(len(items), 165, items)
+    #  b = backpack_brute_force(len(items), 165, items)
+    #  c = backpack_divide_conquer(len(items), 165, items)
+    #  print(a)
+    #  print(b)
+    #  print(c)
+
+    p2 = [1, 2, 3]
+    v2 = [3, 2, 1]
+    i2 = list(zip(p2, v2))
+    d = backpackPD(len(i2), 2, i2)
+    print(d)
 
 main()
